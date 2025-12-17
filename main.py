d151ad911bf58e8f49713beebff445ec6cd0a55e
Maksim Volgin <maxvog2020@gmail.com>
2025-12-17 12:55:10 +0800

    Implement user verification function and update user database format

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
-# TODO
+# TODO: load users from database/users.txt
+
+def verify_user(id, password):
+    return True  # TODO: verify
+
+print("Welcome to <...>!\n")
 
 id = input("Enter your ID: ")
 password = input("Enter your password: ")
 
+if not verify_user(id, password):
+    exit("Invalid ID or password")
+
+name = "Temp name"
+print(f"Welcome, {name}!")
+
+print("\nBooking facilities:")
